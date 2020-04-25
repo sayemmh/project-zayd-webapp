4bda4bce30e9fbdac8346bd8738e79f79f4cdf96
Sayem Hoque <sayemmh@gmail.com>
2020-04-25 13:43:28 -0400

    change dir

--- server.js
+++ server.js
@@ -40,8 +40,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
+
 }
 
 const port = process.env.PORT || 5000;
